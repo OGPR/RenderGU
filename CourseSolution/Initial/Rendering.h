f8a981af1c3095c3a8b4309ab1ff52aa2e966b3c
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-08-21 13:34:55 +0930

    Hello point!

---
+++ CourseSolution/Initial/Rendering.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <GLAD/glad.h> // to get OpenGL includes
+#include <iostream>
+
+
+//macro to write shader programs inline
+#ifndef GLSL 
+#define GLSL(version,A) "#version " #version "\n" #A
+#endif
+
+
+float vertex[] =
+{
+     0.0f, 0.0f, 0.0f
+};
+
+void render_setup()
+{
+
+
+
+
+
+
+
+
+
+
+
+}
+void render_draw()
+{
+    //// Drawing
+
+}
+
