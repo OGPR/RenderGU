51f75fdc3bcc63ac0b33628c0748b955f10c6d2b
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-09-13 19:58:52 +0930

    Index drawn, barycentric interp rect

--- Linux/Eclipse/Projects/src/data.h
+++ Linux/Eclipse/Projects/src/data.h
@@ -14,3 +14,20 @@ float triangle[3*6] =
 	 0.f, 1.f, 0.f,
 	 0.f, 0.f, 1.f,
 };
+
+float rectangle[] =
+{
+    -0.5f,-0.5f,0.f, // bottom left
+     0.5f,-0.5f,0.f, // bottom right
+    -0.5f, 0.5f,0.f, // top left
+     0.5f, 0.5f,0.f, // top right
+	 1.f, 0.f, 0.f,    // Colors - only 3 as we want both triangles that will make this rect to have the same
+	 0.f, 1.f, 0.f,
+	 0.f, 0.f, 1.f,
+};
+
+unsigned int indexArray[3*2] =
+{
+    0, 1, 3, // first triangle
+    0, 2, 3, // second triangle
+};
--- Linux/Eclipse/Projects/src/main.cpp
+++ Linux/Eclipse/Projects/src/main.cpp
@@ -73,9 +73,16 @@ int main()
         compileVertexShader(vertexShaderSource_Tri),
         compileFragmentShader(fragmentShaderSource_Tri));
 
+    unsigned int shaderProgram_Rect = linkShaders(
+        compileVertexShader(vertexShaderSource_Rect),
+        compileFragmentShader(fragmentShaderSource_Rect));
+
 
     unsigned int VAO = render_setup(vertex, 3);
     unsigned int VAO_Triangle = render_setup_tri(triangle, 3*6);
+
+    unsigned int EBO = render_setup_rect(rectangle, 21);
+
 
 
     // Game loop
@@ -107,7 +114,8 @@ int main()
         {
             colorChannelValuesIdx = ++colorChannelValuesIdx % 8;
         }
-        render_draw(shaderProgram_Tri, VAO_Triangle, nullptr, true);
+        //render_draw(shaderProgram_Tri, VAO_Triangle, nullptr, true);
+        render_draw_indexArray(shaderProgram_Rect, EBO);
 
         //// check and call events, and swap buffers
         PollEvents();
