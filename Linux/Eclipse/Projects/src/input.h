871fd565b9bef20de6577eaa1cbac7f3836497d8
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-10-16 11:11:02 +1030

    Yaw left (working WIP)

--- Linux/Eclipse/Projects/src/input.h
+++ Linux/Eclipse/Projects/src/input.h
@@ -8,10 +8,9 @@
 // Now we have +3 as we are actually moving a camera into the direction we want (+z, out of the scree),
 // and keeping the scene fixed. Swapping neg/pos applies to y and x also
 glm::vec3 cameraPosHome = glm::vec3(0.f, 0.f, 3.f);
-glm::vec3 cameraLookAtHome = glm::vec3(0.f, 0.f, 0.f);
+glm::vec3 cameraLookAtHome = glm::vec3(0.f, 0.f, 1.f);
 glm::vec3 cameraPos = cameraPosHome;
-glm::vec3 cameraFront = glm::vec3(0.f, 0.f, -1.f);
-glm::vec3 cameraLookAt = cameraLookAtHome;
+glm::vec3 cameraFront = glm::vec3(0.f, 0.f, 1.f);
 glm::vec3 cameraMoveStep = glm::vec3(1.f, 1.f, 1.f);
 float radius = cameraPosHome.z;
 glm::vec3 cameraCurrRotAngle = glm::vec3(glm::half_pi<float>(), 0.f, 0.f);
@@ -85,30 +84,57 @@ void processInput(GLFWwindow *window, float deltaTime)
     	texture2Amount = 0.2f;
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
     	cameraPos.z -= cameraMoveStep.z * deltaTime;
+    }
 
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
     	cameraPos.z += cameraMoveStep.z * deltaTime;
+    }
 
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
     {
     	cameraPos.x -= cameraMoveStep.x * deltaTime;
-    	cameraLookAt.x = cameraPos.x;
     }
 
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
     	cameraPos.x += cameraMoveStep.x * deltaTime;
-    	cameraLookAt.x = cameraPos.x;
     }
 
     if (glfwGetKey(window, GLFW_KEY_KP_4) == GLFW_PRESS
     		|| glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
     {
-    	cameraCurrRotAngle.y += cameraRotateStep.y * deltaTime;
+    	float thetaPrev = cameraCurrRotAngle.y;
+    	cameraCurrRotAngle.y -= cameraRotateStep.y * deltaTime;
+    	float theta = cameraCurrRotAngle.y -thetaPrev;
+    	/*
     	cameraPos.z = radius * sin(cameraCurrRotAngle.x) * cos(cameraCurrRotAngle.y);
     	cameraPos.x = radius * sin(cameraCurrRotAngle.x) * sin(cameraCurrRotAngle.y) ;
     	cameraPos.y = radius * cos(cameraCurrRotAngle.x);
+    	*/
+
+    	// Note this is not a 4x4 in the homogeneous coordinate sense - I only put the extra
+    	// row of zeros (and zero at the start of each row) because I wanted to offset the index
+    	// (i.e [1][1] is what you expect it to be. This will all go away when I use glm mats and vecs
+    	float RotMat_zx[4][4] =
+    	{
+    	    {0, 0, 0, 0},
+    	    {0, cos(theta), 0, -sin(theta)},
+    	    {0, 0, 1, 0},
+    	    {0, sin(theta), 0, cos(theta)},
+    	};
+
+    	glm::vec3 cameraFrontNew;
+    	cameraFrontNew.x =
+    			cameraFront.x * RotMat_zx[1][1] + cameraFront.y * RotMat_zx[1][2] + cameraFront.z * RotMat_zx[1][3];
+    	cameraFrontNew.y =
+    			cameraFront.x * RotMat_zx[2][1] + cameraFront.y * RotMat_zx[2][2] + cameraFront.z * RotMat_zx[2][3];
+    	cameraFrontNew.z =
+    			cameraFront.x * RotMat_zx[3][1] + cameraFront.y * RotMat_zx[3][2] + cameraFront.z * RotMat_zx[3][3];
+
+    	cameraFront = cameraFrontNew;
     }
 
     if (glfwGetKey(window, GLFW_KEY_KP_6) == GLFW_PRESS
@@ -142,21 +168,19 @@ void processInput(GLFWwindow *window, float deltaTime)
     if (glfwGetKey(window, GLFW_KEY_KP_7) == GLFW_PRESS)
     {
     	cameraPos.y += cameraMoveStep.y * deltaTime;
-    	cameraLookAt.y = cameraPos.y;
     }
 
     if (glfwGetKey(window, GLFW_KEY_KP_9) == GLFW_PRESS)
     {
     	cameraPos.y -= cameraMoveStep.y * deltaTime;
-    	cameraLookAt.y = cameraPos.y;
     }
 
     if (glfwGetKey(window, GLFW_KEY_KP_5) == GLFW_PRESS
     		|| glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
     {
     	cameraPos = cameraPosHome;
-    	cameraLookAt = cameraLookAtHome;
     	cameraCurrRotAngle = glm::vec3(glm::half_pi<float>(), 0.f, 0.f);
+    	cameraFront = cameraLookAtHome;
     }
 }
 
