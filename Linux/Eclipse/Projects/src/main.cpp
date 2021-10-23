449aec4ca91341015fd7cc364e1501fcc0e8dad0
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-10-23 10:52:52 +1030

    Black frame clear color
    To be more consistent with what I am following on learnopengl.com
    (easier to compare my results)

--- Linux/Eclipse/Projects/src/main.cpp
+++ Linux/Eclipse/Projects/src/main.cpp
@@ -213,7 +213,8 @@ int main()
 			glDisable(GL_DEPTH_TEST);
 
         // clear results from previous frame (iteration of loop)
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //// rendering (note this has to be after clear!)
