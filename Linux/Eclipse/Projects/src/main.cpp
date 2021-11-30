908e32a101ff1f67dffc90fce468c623d1d6d970
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-11-30 16:02:12 +1030

    RenderGU window title

--- Linux/Eclipse/Projects/src/main.cpp
+++ Linux/Eclipse/Projects/src/main.cpp
@@ -41,7 +41,7 @@ int main()
     WindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-    Window* window = CreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    Window* window = CreateWindow(800, 600, "RenderGU", NULL, NULL);
     if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
