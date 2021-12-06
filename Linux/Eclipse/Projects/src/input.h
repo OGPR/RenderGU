758a6aaf887c9f1991abd5b647d66c2914e60f1f
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-12-06 20:46:01 +1030

    Camera code in own header

--- Linux/Eclipse/Projects/src/input.h
+++ Linux/Eclipse/Projects/src/input.h
@@ -10,24 +10,8 @@
 #include <vector>
 #include <iostream>
 #include <limits.h>
+#include "Camera.h"
 
-// Camera set up
-// Before we had -3 as we were translating the _scene_ back to give the impression of moving.
-// Now we have +3 as we are actually moving a camera into the direction we want (+z, out of the scree),
-// and keeping the scene fixed. Swapping neg/pos applies to y and x also
-glm::vec3 cameraPosHome = glm::vec3(0.f, 0.f, 3.f);
-glm::vec3 cameraLookAtHome = glm::vec3(0.f, 0.f, -1.f);
-glm::vec3 cameraPos = cameraPosHome;
-glm::vec3 cameraLookDirection = glm::vec3(0.f, 0.f, -1.f);
-glm::vec3 cameraMoveStep = glm::vec3(1.f, 1.f, 1.f);
-float radius = cameraPosHome.z;
-glm::vec3 cameraCurrRotAngle = glm::vec3(glm::half_pi<float>(),glm::pi<float>(), 0.f);
-const glm::vec3 cameraRotateStep = glm::vec3(1.f, 1.f, 1.f);
-glm::vec3 cameraUp = glm::vec3(0.f, 1.f, 0.f);
-
-std::vector < std::vector<float> > RotMat_zx;
-std::vector < std::vector<float> > RotMat_zy;
-std::vector < std::vector<float> > RotMat_xy;
 
 static bool vertFlip = false;
 static float texture2Amount = 0.2f;
@@ -58,58 +42,6 @@ static bool isLightDirectional = true;
 static bool isLightPoint = false;
 static bool isLightSpot = false;
 static bool attenuation = false;
-
-//// Camera Functions
-//TODO put into separate camera code
-float CameraYaw = cameraCurrRotAngle.y;
-float CameraPitch = cameraCurrRotAngle.x;
-
-void UpdateCameraLookDirection(glm::vec3* CameraLookDirection,
-                               float *AngleToUpdate,
-                               float Change)
-{
-    *AngleToUpdate += Change;
-
-    CameraLookDirection->x = sin(CameraPitch)*sin(CameraYaw);
-    CameraLookDirection->z = sin(CameraPitch)*cos(CameraYaw);
-    CameraLookDirection->y = cos(CameraPitch);
-}
-void YawLeft(glm::vec3* CameraLookDirection, float deltaTime)
-{
-    CameraYaw += cameraRotateStep.y * deltaTime;
-
-    CameraLookDirection->x = sin(CameraPitch)*sin(CameraYaw);
-    CameraLookDirection->z = sin(CameraPitch)*cos(CameraYaw);
-}
-
-void YawRight(glm::vec3* CameraLookDirection, float deltaTime)
-{
-    CameraYaw -= cameraRotateStep.y *deltaTime;
-
-    CameraLookDirection->x = sin(CameraPitch) * sin(CameraYaw);
-    CameraLookDirection->z = sin(CameraPitch) * cos(CameraYaw);
-}
-
-void PitchUp(glm::vec3* CameraLookDirection, float deltaTime)
-{
-    CameraPitch -= cameraRotateStep.x * deltaTime;
-
-    CameraLookDirection->x = sin(CameraPitch)*sin(CameraYaw);
-    CameraLookDirection->z = sin(CameraPitch)*cos(CameraYaw);
-    CameraLookDirection->y = cos(CameraPitch);
-
-}
-
-void PitchDown(glm::vec3* CameraLookDirection, float deltaTime)
-{
-    CameraPitch += cameraRotateStep.x * deltaTime;
-
-    CameraLookDirection->x = sin(CameraPitch)*sin(CameraYaw);
-    CameraLookDirection->z = sin(CameraPitch)*cos(CameraYaw);
-    CameraLookDirection->y = cos(CameraPitch);
-
-}
-////
 
 void processInput(GLFWwindow *window, float deltaTime)
 {
