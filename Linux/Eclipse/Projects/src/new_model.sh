8f842daf8bc47d23ab434a471c6f027a6ca00d90
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-12-09 19:33:50 +1030

    Add new model and required files with script
    Added empty simple quad files in this commit.

---
+++ Linux/Eclipse/Projects/src/new_model.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p models/"$1"
+touch models/"$1"/data.h
+touch models/"$1"/model.h
+touch models/"$1"/render.h
+touch models/"$1"/shaders.h
+touch models/"$1"/vertex_specification.h
