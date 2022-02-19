80469c717074e52a9ed8ddb841109950f6c5ed26
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-02-19 15:07:58 +1030

    Frame rate independent 2D cube fade in then out effect

--- Linux/src/main.cpp
+++ Linux/src/main.cpp
@@ -13,10 +13,10 @@
 #endif
 
 #include "Window.h"
-#include "Rendering_Common.h"
 #include "Utility.h"
 #include "CompileShaders.h"
 #include "LinkShaders.h"
+#include "DeltaTime.h"
 
 // To resize viewport whenever window is resized - define a callback (with following signature)
 void framebuffer_size_callback(GLFWwindow* window, int newWidth, int newHeight)
@@ -49,9 +49,11 @@ int main()
         GLSL(330 core,
         out vec4 FragColor;
 
+        uniform float multiplier;
+
         void main()
         {
-            FragColor = vec4(0.2f, 0.7f, 0.1f, 1.0f);
+            FragColor = vec4(multiplier * vec3(1.0f), 1.0f);
         }
         );
 
@@ -70,6 +72,13 @@ int main()
     specifyVertices(cube2D, 18); 
 
 
+    // Fade effect variables 
+    float colorAmount = 0.0f;
+    bool fadeIn = true;
+
+
+
+
     while(!WindowShouldClose(window))
     {
         // Process Input
@@ -82,6 +91,21 @@ int main()
         // Draw to screen
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(ShaderProgram);
+
+        if (fadeIn)
+        {
+            colorAmount += DeltaTime() * 0.2f;
+            if (colorAmount > 1.0f) // To delay the fade out effect (i.e stay at full color), change this (e.g to 1.5f)
+                fadeIn = false;
+        }
+        else
+        {
+            colorAmount -= DeltaTime() * 0.2f;
+        }
+
+
+
+        glUniform1f(glGetUniformLocation(ShaderProgram, "multiplier"), colorAmount); 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
