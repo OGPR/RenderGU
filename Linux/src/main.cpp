923d36055054cf741fd64cb68d3068cbeea5f262
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-08-08 20:24:20 +0930

    Framebuffer size callback in Utility header

--- Linux/src/main.cpp
+++ Linux/src/main.cpp
@@ -31,15 +31,6 @@
 #include "engine/EngineEndFrame.h"
 #include "engine/EngineTickGame.h"
 
-
-// To resize viewport whenever window is resized - define a callback (with following signature)
-void framebuffer_size_callback(GLFWwindow* window, int newWidth, int newHeight)
-{
-    //Note--- this will change viewport location to have lower left corner at (0,0) (how to change this?)
-    glViewport(0, 0, newWidth, newHeight);
-}
-
-
 int main()
 {
     // Create main window
