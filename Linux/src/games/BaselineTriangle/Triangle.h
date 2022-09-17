fe8d03439e551b676c931f33d8ffbc9af436a347
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-09-17 12:09:54 +0930

    EngineBasicShape::Triangle top center texture coord adjusted
    For top-center of image

--- Linux/src/games/BaselineTriangle/Triangle.h
+++ Linux/src/games/BaselineTriangle/Triangle.h
@@ -13,7 +13,7 @@ struct EngineBasicShapes
              0.0f,  1.0f, 0.0f,
              1.0f, -1.0f, 0.0f,
              1.0f, 0.0f,
-             0.0f, 1.0f,
+             0.5f, 1.0f,
              0.0f, 0.0f
         };
 
