8c571d07ff0b21904d526cae65103009719cd892
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-12-13 16:29:45 +1030

    Reinstate shader src and binary
    Back to what it was - prior changes were for error handling testing.

--- src/test/Transformations/shaders/Triangle/src/2DTransformationTest.vert.glsl
+++ src/test/Transformations/shaders/Triangle/src/2DTransformationTest.vert.glsl
@@ -2,12 +2,11 @@
 
 layout(location = 0) in vec3 inPos;
 
-//layout(location = 100) uniform mat4 ModelMatrix;
-//layout(location = 101) uniform mat4 ViewMatrix;
-//layout(location = 102) uniform mat4 ProjectionMatrix;
+layout(location = 100) uniform mat4 ModelMatrix;
+layout(location = 101) uniform mat4 ViewMatrix;
+layout(location = 102) uniform mat4 ProjectionMatrix;
 
 void main()
 {
-    //gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(inPos, 1.0f);
-    gl_Position = vec4(inPos, 1.0f);
+    gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(inPos, 1.0f);
 }
